b2aad2f015148b02ac1fd2d97ce5d82e5287c4f9
xiao278 <90475159+xiao278@users.noreply.github.com>
2024-12-19 16:28:53 +0800

    Initial commit

---
+++ src/main/java/kx/myfirstmod/ModItems.java
@@ -0,0 +1,34 @@
+package kx.myfirstmod;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
+                .register((itemGroup) -> itemGroup.add(ModItems.LIGHTNING_STICK));
+    }
+
+    public static Item register(Item item, String id) {
+        // Create the identifier for the item.
+        Identifier itemID = Identifier.of(MyFirstMod.MOD_ID, id);
+
+        // Register the item.
+        Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
+
+        // Return the registered item!
+        return registeredItem;
+    }
+
+    public static final Item LIGHTNING_STICK = register(
+            new LightningStick(new Item.Settings()
+                    .maxCount(1)
+                    .fireproof()
+            ),
+            "lightning_stick"
+    );
+}
---
+++ src/main/java/kx/myfirstmod/MyFirstMod.java
@@ -0,0 +1,23 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package kx.myfirstmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyFirstMod implements ModInitializer {
+	public static final String MOD_ID = "myfirstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger("myfirstmod");
+
+	public MyFirstMod() {
+	}
+
+	@Override
+	public void onInitialize() {
+		ModItems.initialize();
+	}
+}
