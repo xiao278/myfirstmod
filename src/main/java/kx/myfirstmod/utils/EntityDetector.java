55e8393c618aba23921889b204e30b9ac35513a6
xiao278 <90475159+xiao278@users.noreply.github.com>
2025-01-19 14:32:08 -0500

    reworked bounding box raycast detector to a class of its own

--- src/main/java/kx/myfirstmod/utils/EntityDetector.java
+++ src/main/java/kx/myfirstmod/utils/EntityDetector.java
@@ -99,35 +99,7 @@ public class EntityDetector {
         Vec3d dir = source.getRotationVector();
         Box box = target.getBoundingBox();
         // r.dir is unit direction vector of ray
-        Vec3d dirfrac = new Vec3d(1 / dir.x, 1 / dir.y, 1 / dir.z);
-        // lb is the corner of AABB with minimal coordinates - left bottom, rt is maximal corner
-        // r.org is origin of ray
-        double t1 = (box.minX - origin.x) * dirfrac.x;
-        double t2 = (box.maxX - origin.x) * dirfrac.x;
-        double t3 = (box.minY - origin.y) * dirfrac.y;
-        double t4 = (box.maxY - origin.y) * dirfrac.y;
-        double t5 = (box.minZ - origin.z) * dirfrac.z;
-        double t6 = (box.maxZ - origin.z) * dirfrac.z;
-
-        double tmin = Math.max(Math.max(Math.min(t1, t2), Math.min(t3, t4)), Math.min(t5, t6));
-        double tmax = Math.min(Math.min(Math.max(t1, t2), Math.max(t3, t4)), Math.max(t5, t6));
-
-        // if tmax < 0, ray (line) is intersecting AABB, but the whole AABB is behind us
-        if (tmax < 0)
-        {
-//            double t = tmax;
-            return false;
-        }
-
-        // if tmin > tmax, ray doesn't intersect AABB
-        if (tmin > tmax)
-        {
-//            double t = tmax;
-            return false;
-        }
-
-//        double t = tmin;
-        return true;
+        return BoundingBox.rayIntersection(box, dir, origin).intersectsBox;
     }
 
     public static Vec3d getCenterOffset(Entity e) {
